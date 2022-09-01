b9362d1c8d92407af65769bc53a9c20112aaefad
coxMD <coxmusyoki@gmail.com>
2022-09-01 21:46:33 +0300

    Added frontend and backend files

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=cox
+export POSTGRES_PASSWORD=microservice
+export POSTGRES_HOST=database-1.c8rmnw4gyzhk.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=test-779266096066-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
